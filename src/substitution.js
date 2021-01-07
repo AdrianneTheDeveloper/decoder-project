d65e87bf583015107c70ea481a7c47badaab881c
AdrianneTheDeveloper <meredith.adri@gmail.com>
2021-01-07 16:13:36 -0600

    Substitution code.

--- src/substitution.js
+++ src/substitution.js
@@ -1,45 +1,112 @@
-function substitution(input, alphabet, encode = true) {}
+
 
 const subAlpha = 'xzgbstiojufmrlqcvnkweyapdh';
-const alphaArr = ['a', 'b', 'c'];
+
 
 function substitution(input, alphabet, encode = true) {
 
     try {
-        if (typeof alphabet !== 'string') throw `${alphabet} is not a string.`
-        if (typeof input !== 'string') throw `${input} is not a string.`
+        if (typeof alphabet !== 'string') throw `Alphabet must be a string.`; // Throw an error if alphabet is not a string
+        if (typeof input !== 'string') throw `Input must be a string.`; // Throw an error if input is not a string
+        if (alphabet.length !== 26) throw false; // Throw an error if alphabet is not 26 characters
+        const checkForRepeated = alphabet.toLowerCase().split("").sort().join("").match(/(.)\1+/g); // Check for duplicate characters
+        if (checkForRepeated !== null) throw `Alphabet must not have duplicate letters.` // If checkForRepeated is empty, throw an error
     }
     catch (error) {
-        return `Error: ${error}`
+        return error
     }
 
+    let Msg = ''; // Declare variable for encoded/decoded msg
 
-    input = input.toLowerCase();
-    const alphabetArr = alphabet.split("");
-    const origAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    let inputArr = input.split('');
-    let cypher = [];
+
+    input = input.toLowerCase(); // Change all inputs to lowercase
+    const alphabetArr = alphabet.split(""); // Split alphabet into an array
+    const origAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // Declare array for actual alphabet
+    let inputArr = input.split(''); // Split input into an array by letter
+    let cypher = []; // Declare variable for cypher array of objects
     
-    origAlpha.forEach((alpha, i) => cypher.push({ [alpha]: alphabetArr[i] }));
-    //inputArr = inputArr.map((string) => [string]);
-
-
-    let matchingValues = [];
-    for (let i = 0; i < inputArr.length; i++) {
-        for (let j = 0; j < cypher.length; j++) {
-            if (cypher[j].hasOwnProperty(inputArr[i]))
-                matchingValues.push(cypher[j])
-        }
-    } 
-
-    let encodedMsg = '';
-    matchingValues.forEach((obj, i) => {
-        for (let key in obj) {
-            encodedMsg += obj[key];
-        }
-    })
+    origAlpha.forEach((alpha, i) => cypher.push({ [alpha]: alphabetArr[i] })); // Create cypher (array of objects)
     
-    return encodedMsg
+    
+
+
+    if (encode === true && !/\s/.test(input)) { // If encode is true and there are no spaces in input
+        for (let inputInd = 0; inputInd < inputArr.length; inputInd++) { // Loop through inputArr
+            for (let cypherInd = 0; cypherInd < cypher.length; cypherInd++) { // Loop through cypher
+                let letter = inputArr[inputInd]; // Declare variable for inputArr index
+                let key = Object.keys(cypher[cypherInd]).toString(); // Declare variable for cypher index's key made into a string
+                let property = cypher[cypherInd][key]; // Declare variable for cypher index's property
+                
+                if (letter === key) Msg += property // If letter = key add corresponding property to Msg
+            
+            
+                
+            }
+        }
+    } else if (encode === true && /\s/.test(input)) { // If encode is true and there are spaces in input
+        
+       let inputArr2 = input.split(' ') // Split string inside array into new array with one space in between each letter [[s,t,r,i,n,g], [s,t,r,i,n,g]]
+        let msgArray = [] // Declare variable to push encoded values into
+        inputArr2.forEach((element => { // Loop through each element in array
+            let encoded = []; // Declare variable to push each encoded string into
+            for (let wordInd = 0; wordInd < element.length; wordInd++) {  // Loop through each word 
+                for (let cypherInd = 0; cypherInd < cypher.length; cypherInd++) { // Loop through cypher
+                    
+                    let key = Object.keys(cypher[cypherInd]); // Declare variable for object key
+                    if (element[wordInd] == Object.keys(cypher[cypherInd])) encoded.push(cypher[cypherInd][key]) // If word index matches object key index, push corresponding property into encoded array
+                    
+                    
+                }
+                
+            }
+            msgArray.push(encoded) // Push each encoded array into msgArray
+        }))
+       
+        for (let arrInd = 0; arrInd < msgArray.length; arrInd++) {
+            msgArray[arrInd] = msgArray[arrInd].join('') // Loop through msgArray and join each element together into one word
+        }
+        Msg += msgArray.join(' ') // Join each element with a space and add to Msg
+    }
+
+    if (encode === false && !/\s/.test(input)) { // If encode is false and there are no spaces in input
+        for (let inputInd = 0; inputInd < inputArr.length; inputInd++) { // Loop through inputArr
+            for (let cypherInd = 0; cypherInd < cypher.length; cypherInd++) { // Loop through cypher
+                let letter = inputArr[inputInd]; // Declare variable for inputArr index
+                let key = Object.keys(cypher[cypherInd]).toString(); // Declare variable for cypher index's key made into a string
+                let property = cypher[cypherInd][key]; // Declare variable for cypher index's property
+                
+                if (letter === property) Msg += key // If letter = property add corresponding key to Msg
+            
+            
+                
+            }
+        }
+    } else if (encode === false && /\s/.test(input)) {
+        let inputArr2 = input.split(' ') // Split string inside array into new array with one space in between each letter [[s,t,r,i,n,g], [s,t,r,i,n,g]]
+        let msgArray = [] // Declare variable to push encoded values into
+        inputArr2.forEach((element => { // Loop through each element in array
+            let encoded = []; // Declare variable to push each encoded string into
+            for (let wordInd = 0; wordInd < element.length; wordInd++) {  // Loop through each word 
+                for (let cypherInd = 0; cypherInd < cypher.length; cypherInd++) { // Loop through cypher
+                    
+                    let key = Object.keys(cypher[cypherInd]); // Declare variable for object key
+                    if (element[wordInd] == cypher[cypherInd][key]) encoded.push(Object.keys(cypher[cypherInd])) // If word index matches object property index, push corresponding key into encoded array
+                    
+                    
+                }
+                
+            }
+            msgArray.push(encoded) // Push each encoded array into msgArray
+        }))
+       
+        for (let arrInd = 0; arrInd < msgArray.length; arrInd++) {
+            msgArray[arrInd] = msgArray[arrInd].join('') // Loop through msgArray and join each element together into one word
+        }
+        Msg += msgArray.join(' ') // Join each element with a space and add to Msg
+    }
+
+    return Msg
+    
 }
-console.log(substitution('Adrianne', subAlpha))
+console.log(substitution('oj xbnjxlls', subAlpha, false)) 
 module.exports = substitution;
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -6,18 +6,46 @@ const expect = require("chai").expect;
 
 const subAlpha = 'xzgbstiojufmrlqcvnkweyapdh';
 
-/*describe('substitution', () => {
+describe('substitution', () => {
     it('should be a function', () => {
         expect(substitution).to.be.a('function')
     })
     it('should throw an error if if alphabet is not a string', () => {
-        const expected = `Error: a,b,c is not a string.`
+        const expected = `Alphabet must be a string.`
         const actual = substitution('Hello', ['a', 'b', 'c'])
         expect(expected).to.equal(actual)
     })
     it('should throw an error is input is not a string', () => {
-        const expected = `Error: Hello is not a string.`
+        const expected = `Input must be a string.`
         const actual = substitution(['Hello'], subAlpha)
         expect(expected).to.equal(actual)
     })
-})*/
+    it(`should throw an error if alphabet doesn't have 26 characters`, () => {
+        const expected = false
+        const actual = substitution('Hello', 'xzg!bstiojufmrlqcvnkweyapdh')
+        expect(expected).to.equal(actual)
+    })
+    it(`should throw an error is there are duplicate letters in alphabet`, () => {
+        const expected = `Alphabet must not have duplicate letters.`;
+        const actual = substitution('Hello', 'xzzbstiojufmrlqcvnkweyapdh');
+        expect(expected).to.equal(actual)
+    })
+    it(`should return an encoded message when encode is true`, () => {
+        const expected = 'xbnjxlls';
+        const actual = substitution('Adrianne', subAlpha);
+        expect(expected).to.equal(actual);
+    })
+    it(`it should decode inputted messages when encode is false`, () => {
+        const expected = 'adrianne';
+        const actual = substitution('xbnjxlls', subAlpha, false)
+    })
+    it(`should maintain spaces when encoding or decoding messages`, () => {
+        const expected = 'oj xbnjxlls';
+        const expected2 = 'hi adrianne';
+        const actual = substitution('Hi Adrianne', subAlpha);
+        const actual2 = substitution('oj xbnjxlls', subAlpha, false);
+
+        expect(expected).to.equal(actual);
+        expect(expected2).to.equal(actual2);
+    })
+})
